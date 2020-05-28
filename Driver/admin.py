9f0a62116f20509705bc2b44d3121ce849116a18
Keshav Bathla <keshavbathla2017@gmail.com>
2020-05-28 14:42:09 +0530

    Add Driver Tables

--- Driver/admin.py
+++ Driver/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+	Driver, DriverNotification
+)
+
+@admin.register(Driver)
+class DriverAdmin(admin.ModelAdmin):
+	list_display = ('user', 'shop_assigned', 'is_free', 'id')
+	search_fields = ('user__name', 'user__email', 'user__mobile', 'user__id',
+		'shop_assigned', 'is_free', 'id')
+
+@admin.register(DriverNotification)
+class DriverNotificationAdmin(admin.ModelAdmin):
+	list_display = ('driver', 'date', 'time', 'notification_text', 'id')
+	search_fields = (
+		'driver__user__name', 'driver__user__email', 'driver__user__mobile', 'driver__id',
+		'time', 'notification_text', 'id')
--- Driver/models.py
+++ Driver/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+from User import models as user_models
+from Shop import models as shop_models
+
+class Driver(models.Model):
+    user = models.ForeignKey(user_models.User, on_delete=models.CASCADE, related_name="driver_driver_user", null=True, blank=True)
+    shop_assigned = models.ForeignKey(shop_models.Shop, on_delete=models.CASCADE, related_name="driver_driver_shop", null=True, blank=True)
+    is_free = models.BooleanField(default=False, null=True, blank=True)
+
+    created = models.DateTimeField(auto_now_add=True, editable=False)
+    last_updated = models.DateTimeField(auto_now=True, editable=False)
+
+    class Meta:
+        verbose_name = 'Driver'
+        verbose_name_plural = 'Drivers'
+    def __str__(self):
+        return self.user.name
+
+
+class DriverNotification(models.Model):
+    driver = models.ForeignKey(Driver, on_delete=models.CASCADE, related_name="driver_driver_notification_client", null=True, blank=True)
+    date = models.DateField(null=True, blank=True)
+    time = models.TimeField(null=True, blank=True)
+    notification_text = models.TextField(null=True, blank=True)
+
+    created = models.DateTimeField(auto_now_add=True, editable=False)
+    last_updated = models.DateTimeField(auto_now=True, editable=False)
+
+    class Meta:
+        verbose_name = 'Driver Notification'
+        verbose_name_plural = 'Driver Notifications'
