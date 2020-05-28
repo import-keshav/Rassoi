2417e667569b1903dd3a49ced12da1c34bf66971
Keshav Bathla <keshavbathla2017@gmail.com>
2020-05-28 13:47:02 +0530

    Added Shop Models

--- Shop/admin.py
+++ Shop/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 
 from .models import (
-	Shop,
-	ShopPromocode,
+	Shop, ShopPromocode, Grocery, Fruit, Vegetable, FoodPackage,
+	FoodDishes, Slots
 )
 
 @admin.register(Shop)
 class ShopAdmin(admin.ModelAdmin):
-	list_display = ('location_coordinates', 'is_open', 'address', 'image', 'id')
+	list_display = ('address', 'location_coordinates', 'is_open', 'image', 'id')
 	search_fields = ('location_coordinates', 'address', 'id',)
 
 
@@ -15,3 +15,36 @@ class ShopAdmin(admin.ModelAdmin):
 class ShopPromocodeAdmin(admin.ModelAdmin):
 	list_display = ('promocode', 'shop', 'discount_percentage', 'valid_date', 'id')
 	search_fields = ('id', 'shop__id', 'shop__name', 'promocode', 'valid_date')
+
+
+@admin.register(Grocery)
+class GroceryAdmin(admin.ModelAdmin):
+	list_display = ('name', 'shop', 'price_per_item', 'price_per_kg',
+		'is_available', 'id')
+	search_fields = ('name', 'shop', 'id',)
+
+
+@admin.register(Fruit)
+class FruitAdmin(admin.ModelAdmin):
+	list_display = ('name', 'shop', 'price_per_dozen', 'price_per_kg',
+		'is_available', 'id')
+	search_fields = ('name', 'shop', 'id',)
+
+
+@admin.register(Vegetable)
+class FruitAdmin(admin.ModelAdmin):
+	list_display = ('name', 'shop', 'price_per_kg', 'is_available', 'id')
+	search_fields = ('name', 'shop', 'id',)
+
+
+class FoodDishesTabularInline(admin.TabularInline):
+	model = FoodDishes
+	extra = 1
+
+
+@admin.register(FoodPackage)
+class FoodPackageAdmin(admin.ModelAdmin):
+	list_display = ('name', 'shop', 'price_per_meal', 'price_per_month',
+		'is_available', 'id')
+	search_fields = ('name', 'shop', 'id',)
+	inlines = (FoodDishesTabularInline,)
--- Shop/models.py
+++ Shop/models.py
@@ -2,6 +2,19 @@ from django.db import models
 from django.core.validators import MinValueValidator
 
 
+ITEMS = (
+    ('Grocery', 'Grocery'),
+    ('Fruit', 'Fruit'),
+    ('Vegetable', 'Vegetable'),
+    ('Food', 'Food')
+)
+
+FOOD_TYPE = (
+    ('Breakfast', 'Breakfast'),
+    ('Lunch', 'Lunch'),
+    ('Dinner', 'Dinner'),
+
+)
 class Shop(models.Model):
     location_coordinates = models.CharField(max_length=30, null=True, blank=True)
     is_open = models.BooleanField(default=False, null=True, blank=True)
@@ -15,6 +28,89 @@ class Shop(models.Model):
         verbose_name_plural = 'Shops'
     def __str__(self):
         return self.address
+
+
+class Grocery(models.Model):
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE, related_name="shop_grocery_shop", null=True, blank=True)
+    name = models.CharField(max_length=100, null=True, blank=True)
+    image = models.FileField(null=True, blank=True, upload_to="")
+    price_per_item = models.FloatField(null=True, blank=True, validators=[MinValueValidator(0)])
+    price_per_kg = models.FloatField(null=True, blank=True, validators=[MinValueValidator(0)])
+    is_available = models.BooleanField(default=False, null=True, blank=True)
+
+    created = models.DateTimeField(auto_now_add=True, editable=False)
+    last_updated = models.DateTimeField(auto_now=True, editable=False)
+    class Meta:
+        verbose_name = 'Grocery'
+        verbose_name_plural = 'Groceries'
+    def __str__(self):
+        return self.name
+
+
+class Fruit(models.Model):
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE, related_name="shop_fruit_shop", null=True, blank=True)
+    name = models.CharField(max_length=100, null=True, blank=True)
+    image = models.FileField(null=True, blank=True, upload_to="")
+    price_per_dozen = models.FloatField(null=True, blank=True, validators=[MinValueValidator(0)])
+    price_per_kg = models.FloatField(null=True, blank=True, validators=[MinValueValidator(0)])
+    is_available = models.BooleanField(default=False, null=True, blank=True)
+
+    created = models.DateTimeField(auto_now_add=True, editable=False)
+    last_updated = models.DateTimeField(auto_now=True, editable=False)
+    class Meta:
+        verbose_name = 'Fruit'
+        verbose_name_plural = 'Fruites'
+    def __str__(self):
+        return self.name
+
+
+class Vegetable(models.Model):
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE, related_name="shop_vegetable_shop", null=True, blank=True)
+    name = models.CharField(max_length=100, null=True, blank=True)
+    image = models.FileField(null=True, blank=True, upload_to="")
+    price_per_kg = models.FloatField(null=True, blank=True, validators=[MinValueValidator(0)])
+    is_available = models.BooleanField(default=False, null=True, blank=True)
+
+    created = models.DateTimeField(auto_now_add=True, editable=False)
+    last_updated = models.DateTimeField(auto_now=True, editable=False)
+    class Meta:
+        verbose_name = 'Vegetable'
+        verbose_name_plural = 'Vegetables'
+    def __str__(self):
+        return self.name
+
+
+class FoodPackage(models.Model):
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE, related_name="shop_food_shop", null=True, blank=True)
+    name = models.CharField(max_length=100, null=True, blank=True)
+    details = models.TextField(null=True, blank=True)
+    food_type = models.TextField(choices=FOOD_TYPE, null=True, blank=True)
+    image = models.FileField(null=True, blank=True, upload_to="")
+    price_per_meal = models.FloatField(null=True, blank=True, validators=[MinValueValidator(0)])
+    price_per_month = models.FloatField(null=True, blank=True, validators=[MinValueValidator(0)])
+    is_available = models.BooleanField(default=False, null=True, blank=True)
+
+    created = models.DateTimeField(auto_now_add=True, editable=False)
+    last_updated = models.DateTimeField(auto_now=True, editable=False)
+    class Meta:
+        verbose_name = 'Food Package'
+        verbose_name_plural = 'Food Packages'
+    def __str__(self):
+        return self.name
+
+
+class FoodDishes(models.Model):
+    package = models.ForeignKey(FoodPackage, on_delete=models.CASCADE, related_name="shop_food_dishes_package", null=True, blank=True)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    number_of_items = models.FloatField(null=True, blank=True, validators=[MinValueValidator(0)])
+
+    created = models.DateTimeField(auto_now_add=True, editable=False)
+    last_updated = models.DateTimeField(auto_now=True, editable=False)
+    class Meta:
+        verbose_name = 'Food Dishe'
+        verbose_name_plural = 'Food Dishes'
+    def __str__(self):
+        return self.name
 
 
 class ShopPromocode(models.Model):
@@ -31,3 +127,19 @@ class ShopPromocode(models.Model):
         verbose_name_plural = 'Shop Promocodes'
     def __str__(self):
         return self.promocode
+
+
+class Slots(models.Model):
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE, related_name="shop_slots_shop", null=True, blank=True)
+    category = models.TextField(choices=ITEMS, null=True, blank=True)
+    start_time = models.TimeField(null=True, blank=True)
+    end_time = models.TimeField(null=True, blank=True)
+    date = models.DateField(null=True, blank=True)
+
+    created = models.DateTimeField(auto_now_add=True, editable=False)
+    last_updated = models.DateTimeField(auto_now=True, editable=False)
+    class Meta:
+        verbose_name = 'Slot'
+        verbose_name_plural = 'Slots'
+    def __str__(self):
+        return self.start_time
