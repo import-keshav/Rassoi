e048f5cc8a4ba4121f16da46320147cbe3d15432
Keshav Bathla <keshavbathla2017@gmail.com>
2020-07-11 12:34:38 +0530

    Fix List Driver API

--- Driver/serializers.py
+++ Driver/serializers.py
@@ -2,8 +2,10 @@ from django import forms
 from rest_framework import serializers
 
 from . import models as driver_models
+from User import serializers as user_serializer
 
 class ListDriverSerializer(serializers.ModelSerializer):
+    user = user_serializer.GetUserInfoSerializer()
     class Meta:
         model = driver_models.Driver
         fields = '__all__'
