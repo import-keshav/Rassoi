02bd06f083eea89eed4985eeb1b2a6983464a731
Keshav Bathla <keshavbathla2017@gmail.com>
2020-10-19 22:10:32 +0530

    Fix User Models Save Method

--- User/models.py
+++ User/models.py
@@ -32,11 +32,13 @@ class User(models.Model):
 
     def save(self):
         if not self.pk:
-            self.password = hash_password(self.password)
+            if self.password:
+                self.password = hash_password(self.password)
         else:
             obj = User.objects.get(pk=self.pk)
             if obj.password != self.password:
-                self.password = hash_password(self.password)
+                if self.password:
+                    self.password = hash_password(self.password)
         super(User, self).save()
 
 
